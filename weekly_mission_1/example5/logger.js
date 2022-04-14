c80fb52c5b9ef78db6f7b72daeddd020e479e922
Xo Arsi <xoarci@gmail.com>
2022-04-13 23:57:17 -0500

    Create logger.js

---
+++ weekly_mission_1/example5/logger.js
@@ -0,0 +1,16 @@
+class Logger {
+    constructor(name){
+      // Al crear este objeto se guardarán estos valores
+      this.count = 0
+      this.name = name
+    }
+  
+    log(message) {
+      this.count++ // se aumenta el contador interno al llamar este método
+      console.log('[' + this.name + '] ' + message)
+    }
+  }
+  
+ /* module.exports = new Logger('DEFAULT') // Instanciación del objeto y se exporta*/
+
+ module.exports = new Logger('Xoarsi') // Instanciación del objeto y se exporta
