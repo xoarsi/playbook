571c21ab24049c31042794b6c44a85b6325ef0f4
Xo Arsi <xoarci@gmail.com>
2022-04-14 00:14:42 -0500

    Create patcher.js

---
+++ weekly_mission_1/example6/patcher.js
@@ -0,0 +1,10 @@
+/*
+ * Tener en cuenta:
+ *    - require('./logger') te dará el objeto creado
+ *    - require('./logger').Logger te regresará la clase
+ *
+ * En este caso estamos agregando una función más al objeto instanciado, no a la clase.
+ * */
+require('./logger').customMessage = function () {
+    console.log('This is a new functionality for the object')
+}
