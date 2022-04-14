4278f4535a6ae05d24abdfb6fca7c53aaee875fa
Xo Arsi <xoarci@gmail.com>
2022-04-13 22:51:15 -0500

    Create main.js

---
+++ weekly_mission_1/example2/main.js
@@ -0,0 +1,7 @@
+// node main.js
+
+const logger = require('./logger')
+
+logger.info('This is an informational message')
+logger.verbose('This is a verbose message')
+logger.xoarsi('This is a message written by xoarsi')
