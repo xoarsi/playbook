5f2290d8eba2fae8719f30a2f7a3f62dd71fe26e
Xo Arsi <xoarci@gmail.com>
2022-04-14 10:31:43 -0500

    Create main.js

---
+++ weekly_mission_1/example9/main.js
@@ -0,0 +1,22 @@
+const Pokemon = require('./pokemon')
+
+const pikachu = new Pokemon("pikachu")
+const bulbasaur = new Pokemon("bulbasaur")
+const squirtle = new Pokemon("squirtle")
+const charmander = new Pokemon("charmander")
+const charizard = new Pokemon("charizard")
+
+pikachu.sayHello()
+pikachu.sayMessage("Heey explorer!")
+
+bulbasaur.sayHello()
+bulbasaur.sayMessage("Heey explorer!")
+
+charmander.sayHello()
+charmander.sayMessage("Heey explorer!")
+
+squirtle.sayHello()
+squirtle.sayMessage("Heey explorer!")
+
+charizard.sayHello()
+charizard.sayMessage("Heey explorer!")
